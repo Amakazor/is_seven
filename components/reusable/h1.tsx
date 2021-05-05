7d4bbc7ed16679571109ba9c60efe9d3d9c6e727
amakazor <amakazor@gmail.com>
2021-05-05 20:09:43 +0200

    Create h1.tsx

---
+++ components/reusable/h1.tsx
@@ -0,0 +1,13 @@
+import React, { ReactChild } from 'react';
+import styled, { ThemedStyledProps } from 'styled-components';
+
+const StyledH1 = styled.h1`
+    font-size: 5rem;
+    color: ${(props) => props.theme.colorPrimary};
+    font-weight: 500;
+    margin-bottom: 0;
+`;
+
+export default function H1(props: { children: ReactChild | ReactChild[] }) {
+    return <StyledH1>{props.children}</StyledH1>;
+}

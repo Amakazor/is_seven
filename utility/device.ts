6930afee017ae5e2373112483a5f6b3e965e4ca7
amakazor <amakazor@gmail.com>
2021-05-05 03:16:15 +0200

    Update device.ts

--- utility/device.ts
+++ utility/device.ts
@@ -6,27 +6,27 @@ const device = {
         max: `(max-width: ${size.mobileS.max})`,
     },
     mobileM: {
-        min: `min-width: ${size.mobileM.min})`,
+        min: `(min-width: ${size.mobileM.min})`,
         max: `(max-width: ${size.mobileM.max})`,
     },
     mobileL: {
-        min: `min-width: ${size.mobileL.min})`,
+        min: `(min-width: ${size.mobileL.min})`,
         max: `(max-width: ${size.mobileL.max})`,
     },
     tablet: {
-        min: `min-width: ${size.tablet.min})`,
+        min: `(min-width: ${size.tablet.min})`,
         max: `(max-width: ${size.tablet.max})`,
     },
     laptop: {
-        min: `min-width: ${size.laptop.min})`,
+        min: `(min-width: ${size.laptop.min})`,
         max: `(max-width: ${size.laptop.max})`,
     },
     laptopL: {
-        min: `min-width: ${size.laptopL.min})`,
+        min: `(min-width: ${size.laptopL.min})`,
         max: `(max-width: ${size.laptopL.max})`,
     },
     desktop: {
-        min: `min-width: ${size.desktop.min})`,
+        min: `(min-width: ${size.desktop.min})`,
         max: `(max-width: ${size.desktop.max})`,
     },
 }

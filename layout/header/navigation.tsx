e5eb70bc64229aafa7c6ab88d7c791103ceb5ed1
amakazor <amakazor@gmail.com>
2021-05-05 20:09:22 +0200

    changed bottom border in navigation to always appear

--- layout/header/navigation.tsx
+++ layout/header/navigation.tsx
@@ -1,20 +1,18 @@
 import styled from 'styled-components';
 import Logo from '../../components/header/navigation/logo';
 import Menu from '../../components/header/navigation/menu';
-import device from '../../utility/device';
 
 const StyledNavigation = styled.nav`
     height: 5rem;
     padding: 1rem 2rem;
     position: fixed;
     width: 100%;
+    top: 0;
+    z-index: 9999;
     background: ${(props) => props.theme.colorAccent};
     display: flex;
     justify-content: space-between;
-
-    @media ${device.mobileL.max} {
-        border-bottom: 0.125rem solid ${(props) => props.theme.colorPrimary};
-    }
+    border-bottom: 0.125rem solid ${(props) => props.theme.colorPrimary};
 `;
 
 export default function Navigation() {

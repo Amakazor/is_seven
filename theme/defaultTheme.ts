5d33781cd15bcb868a5e8607b273f96730e07220
amakazor <amakazor@gmail.com>
2021-05-05 20:09:55 +0200

    Changed background color

--- theme/defaultTheme.ts
+++ theme/defaultTheme.ts
@@ -3,7 +3,7 @@ import { DefaultTheme } from "styled-components";
 export const defaultTheme: DefaultTheme = {
     colorPrimary: "#FFFFFF",
     colorAccent: "#408FF1",
-    colorBackground: "#CCCCCC",
+    colorBackground: "#EEEEEE",
     transitionTime: "200ms",
     borderRadius: "0.25rem"
 }

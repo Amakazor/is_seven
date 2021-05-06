59f2d2acdb155e3f873d47f5fdf7a18c8eeb1459
amakazor <amakazor@gmail.com>
2021-05-06 16:58:17 +0200

    formatted index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,25 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Header from '../layout/header';
 import Main from '../layout/main';
+
 import { ThemeProvider } from 'styled-components';
-import Header from "../layout/header";
-import { defaultTheme } from "../theme/defaultTheme"
+import { defaultTheme } from '../theme/defaultTheme';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Is Seven API</title>
-        <meta name="description" content="API to find if number is seven"/>
-        <meta name="author" content="Arkadiusz Wrzawiński"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta name="keywords" content="API, Seven, Number, Is Seven, Joke, Mildly Funny"/>
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
-      <ThemeProvider theme={defaultTheme}>
-        <Header/>
-      </ThemeProvider>
-    </div>
-  )
+    return (
+        <div>
+            <Head>
+                <title>Is Seven API</title>
+                <meta name="description" content="API to find if number is seven" />
+                <meta name="author" content="Arkadiusz Wrzawiński" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="keywords" content="API, Seven, Number, Is Seven, Joke, Mildly Funny" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <ThemeProvider theme={defaultTheme}>
+                <Header />
                 <Main />
+            </ThemeProvider>
+        </div>
+    );
 }

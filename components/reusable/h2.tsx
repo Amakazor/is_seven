c54557f24be687fc1d10ccc903274449790fc7a9
amakazor <amakazor@gmail.com>
2021-05-07 19:03:01 +0200

    Update h2.tsx

--- components/reusable/h2.tsx
+++ components/reusable/h2.tsx
@@ -1,13 +1,21 @@
 import { ReactChild } from 'react';
 import styled from 'styled-components';
+import device from '../../utility/device';
 
-const StyledH1 = styled.h2<{textColor: string}>`
-    font-size: 3rem;
+const StyledH2 = styled.h2<{textColor: string}>`
+    font-size: 3.5rem;
     color: ${(props) => props.textColor};
     font-weight: 500;
     margin: 0;
+    margin-bottom: 0.5em;
+    line-height: 1.1;
+    text-align: center;
+
+    @media ${device.mobileL.max} {
+        font-size: 2.2rem;
+    }
 `;
 
 export default function H2(props: {textColor: string, children: ReactChild | ReactChild[] }) {
-    return <StyledH1 textColor={props.textColor}>{props.children}</StyledH1>;
+    return <StyledH2 textColor={props.textColor}>{props.children}</StyledH2>;
 }

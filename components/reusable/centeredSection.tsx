8aa5d91e7d52067db34a9329f347ae5991845765
amakazor <amakazor@gmail.com>
2021-05-07 19:02:53 +0200

    added possible id attribute to centered section

--- components/reusable/centeredSection.tsx
+++ components/reusable/centeredSection.tsx
@@ -5,11 +5,12 @@ const StyledCenteredSection = styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
+    padding: 1rem 1rem;
 `
 
-export default function CenteredSection(props: {children: ReactChild | ReactChild[]}) {
+export default function CenteredSection(props: {children: ReactChild | ReactChild[], id?: string}) {
     return (
-        <StyledCenteredSection>
+        <StyledCenteredSection id={props.id ?? ""}>
             {props.children}
         </StyledCenteredSection>
     )

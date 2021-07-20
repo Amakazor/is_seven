ee931d86c9794cbe6fd03cb2a50c6c97d5efa209
amakazor <amakazor@gmail.com>
2021-07-21 00:34:24 +0200

    Added Bold component

---
+++ components/reusable/bold.tsx
@@ -0,0 +1,19 @@
+import { ReactChild } from "react";
+import styled from "styled-components";
+
+interface boldProperties {
+    weight: number;
+    children: ReactChild | ReactChild[];
+}
+
+const BoldElement = styled.span<{weight: number}>`
+    font-weight: ${props => props.weight};
+`
+
+export default function Bold(props: boldProperties) {
+    return (
+        <BoldElement weight={props.weight}>
+            {props.children}
+        </BoldElement>
+    )
+}

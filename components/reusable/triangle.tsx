79c06d87afa59dc7091e78e5beab5cdecfa6f69c
amakazor <amakazor@gmail.com>
2021-08-15 14:43:26 +0200

    Half sized and reversed triangle

--- components/reusable/triangle.tsx
+++ components/reusable/triangle.tsx
@@ -3,17 +3,24 @@ import styled from "styled-components";
 
 interface triangleProps {
     backgroundColor: string;
-    isReversed?: boolean
+    isReversed?: boolean;
+    halfSize?: boolean;
 
     children?: ReactChild | ReactChild[];
 }
 
 const StyledTriangle = styled.div<triangleProps>`
     width: 100%;
-    height: 20vw;
+    height: ${props => props.halfSize ? '12vw' : '20vw'};
     position: relative;
     background: ${(props) => props.backgroundColor};
-    clip-path: ${(props) => (!props.isReversed ? 'polygon(0 0, 100% 0, 100% 50%, 50% 100%, 0 50%)' : 'polygon(0 100%, 100% 100%, 100% 0, 50% 50%, 0 0)')};
+    clip-path: ${(props) => props.halfSize
+        ? !props.isReversed 
+            ? 'polygon(0 0, 100% 0, 100% 90%, 50% 100%, 0 90%)' 
+            : 'polygon(0 100%, 100% 100%, 100% 0, 50% 90%, 0 0)'
+        : !props.isReversed 
+            ? 'polygon(0 0, 100% 0, 100% 50%, 50% 100%, 0 50%)' 
+            : 'polygon(0 100%, 100% 100%, 100% 0, 50% 50%, 0 0)'};
     overflow: hidden;
 `
 
